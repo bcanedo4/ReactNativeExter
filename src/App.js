769b4cb65d9537280bd0f15859f5b85b68653cc9
Brian Canedo <bcanedo4@gmail.com>
2018-04-16 12:07:13 -0700

    Included Firebase

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyBsm9EuIEJIXY_KE4yekgPB2O3iG0OaGEQ',
+            authDomain: 'exter-8dff0.firebaseapp.com',
+            databaseURL: 'https://exter-8dff0.firebaseio.com',
+            projectId: 'exter-8dff0',
+            storageBucket: 'exter-8dff0.appspot.com',
+            messagingSenderId: '121786210904'
+          };
+
+          firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>

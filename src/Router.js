924a2dbd63a1ff88d0cae7ff554d8bab6191f87f
Brian Canedo <bcanedo4@gmail.com>
2018-04-19 11:56:58 -0700

    Created Login that successfully signs a user up on firebase

---
+++ src/Router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+
+const RouterComponent = () => (
+        <Router>
+            <Scene key="root" hideNavBar>
+                <Scene key="auth">
+                    <Scene key="login" component={LoginForm} title="Login" initial />
+                </Scene>
+            </Scene>
+        </Router>
+    );
+
+export default RouterComponent;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
+import { Card, CardSection, Input, Button } from './common';
+
+class LoginForm extends Component {
+    onEmailChange(text) {
+        this.props.emailChanged(text);
+    }
+
+    onPasswordChange(text) {
+        this.props.passwordChanged(text);
+    }
+
+    onButtonPress() {
+        const { email, password } = this.props;
+
+        this.props.loginUser({ email, password });
+    }
+
+    renderButton() {
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                Login to EXTER
+            </Button>
+        );
+    }
+
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <Input 
+                        label="Email"
+                        placeholder="email@gmail.com"
+                        onChangeText={this.onEmailChange.bind(this)}
+                        value={this.props.email}
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input 
+                        secureTextEntry
+                        label="Password"
+                        placeholder="password"
+                        onChangeText={this.onPasswordChange.bind(this)}
+                        value={this.props.password}
+                    />
+                </CardSection>
+
+                <CardSection>
+                    {this.renderButton()}
+                </CardSection>
+            </Card>
+        );
+    }
+}
+
+const mapStateToProps = ({ auth }) => {
+    const { email, password, error } = auth;
+
+    return { email, password, error };
+};
+
+export default connect(mapStateToProps, {
+    emailChanged, passwordChanged, loginUser
+})(LoginForm);
